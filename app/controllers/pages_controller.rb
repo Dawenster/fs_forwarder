50fab681aca747230157ca27162a9d11c043adba
David Wen <dstwen@gmail.com>
2014-06-23 22:41:58 -0400

    build link here

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def index
-    redirect_to URI::decode(params[:url])
+    date = Date.parse(params[:date])
+    link = "http://www.travelocity.com/Flights-Search?trip=oneway&leg1=from:#{params[:departure_airport_code]},to:#{params[:arrival_airport_code]},departure:#{date.strftime('%m') + '%2f' + date.strftime('%d') + '%2f' + date.strftime('%Y')}TANYT&passengers=children:0,adults:1,seniors:0,infantinlap:Y&options=cabinclass:coach,nopenalty:N,sortby:price&mode=search"
+    redirect_to link
   end
 end
