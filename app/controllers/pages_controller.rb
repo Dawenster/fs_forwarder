27fbfd373cba5c3bf0cf5438a8af59104e32377c
David Wen <dstwen@gmail.com>
2014-06-27 21:38:31 -0400

    fix params check

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    if params.has_key?(:one) && params.has_key?(:two)
+    if params.has_key?(:departure_airport_code) && params.has_key?(:arrival_airport_code) && params.has_key?(:departure_time)
       date = Date.parse(params[:departure_time])
       @link = "http://www.travelocity.com/Flights-Search?trip=oneway&leg1=from:#{params[:departure_airport_code]},to:#{params[:arrival_airport_code]},departure:#{date.strftime('%m') + '%2f' + date.strftime('%d') + '%2f' + date.strftime('%Y')}TANYT&passengers=children:0,adults:1,seniors:0,infantinlap:Y&options=cabinclass:coach,nopenalty:N,sortby:price&mode=search"
       redirect_to @link
