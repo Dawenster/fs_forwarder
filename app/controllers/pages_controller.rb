7f512aea11e6c054c5b624a4e947d766e324c093
David Wen <dstwen@gmail.com>
2014-06-27 21:30:28 -0400

    attempt js referrer stripper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def index
     date = Date.parse(params[:departure_time])
     @link = "http://www.travelocity.com/Flights-Search?trip=oneway&leg1=from:#{params[:departure_airport_code]},to:#{params[:arrival_airport_code]},departure:#{date.strftime('%m') + '%2f' + date.strftime('%d') + '%2f' + date.strftime('%Y')}TANYT&passengers=children:0,adults:1,seniors:0,infantinlap:Y&options=cabinclass:coach,nopenalty:N,sortby:price&mode=search"
-    redirect_to @link
+    redirect_to @link if params
   end
 end
