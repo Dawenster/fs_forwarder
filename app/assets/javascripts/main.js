d288002a0e3a12a51245067be5cc44fd08c94b0a
David Wen <dstwen@gmail.com>
2014-06-27 22:07:16 -0400

    update js redirect

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  var link = document.getElementById("link").innerHTML;
+  link.replace("amp;", "");
+  var cleanLink = link.replace(/\b(amp;)\b/gi, '');
+  window.location.href = cleanLink;
+}, 3000);
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     if params.has_key?(:departure_airport_code) && params.has_key?(:arrival_airport_code) && params.has_key?(:departure_time)
       date = Date.parse(params[:departure_time])
       @link = "http://www.travelocity.com/Flights-Search?trip=oneway&leg1=from:#{params[:departure_airport_code]},to:#{params[:arrival_airport_code]},departure:#{date.strftime('%m') + '%2f' + date.strftime('%d') + '%2f' + date.strftime('%Y')}TANYT&passengers=children:0,adults:1,seniors:0,infantinlap:Y&options=cabinclass:coach,nopenalty:N,sortby:price&mode=search"
-      redirect_to @link
+      # redirect_to @link
     end
   end
 end
