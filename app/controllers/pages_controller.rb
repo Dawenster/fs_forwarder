aff4a0d6a44d79518d453188207dea962d31a304
David Wen <dstwen@gmail.com>
2014-06-27 21:32:44 -0400

    fix pages controller for if no params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def index
-    date = Date.parse(params[:departure_time])
-    @link = "http://www.travelocity.com/Flights-Search?trip=oneway&leg1=from:#{params[:departure_airport_code]},to:#{params[:arrival_airport_code]},departure:#{date.strftime('%m') + '%2f' + date.strftime('%d') + '%2f' + date.strftime('%Y')}TANYT&passengers=children:0,adults:1,seniors:0,infantinlap:Y&options=cabinclass:coach,nopenalty:N,sortby:price&mode=search"
-    redirect_to @link if params
+    if params.has_key?(:one) && params.has_key?(:two)
+      date = Date.parse(params[:departure_time])
+      @link = "http://www.travelocity.com/Flights-Search?trip=oneway&leg1=from:#{params[:departure_airport_code]},to:#{params[:arrival_airport_code]},departure:#{date.strftime('%m') + '%2f' + date.strftime('%d') + '%2f' + date.strftime('%Y')}TANYT&passengers=children:0,adults:1,seniors:0,infantinlap:Y&options=cabinclass:coach,nopenalty:N,sortby:price&mode=search"
+      redirect_to @link
+    end
   end
 end
